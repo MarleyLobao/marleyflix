4a2b89c7b483bc440b2a377e0f5d166a57fd001c
MarleyLobao <marley.sousa@ee.ufcg.edu.br>
2020-07-28 02:30:54 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../assets/img/logo.png';
+import './Menu.css'
+import Button from '../Button';
+//import ButtonLink from './components/ButtonLink';
+
+function Menu(){
+    return(
+        <nav className="Menu">
+            <a href="/">
+                <img className="logo" src={logo} alt="Marleyflix logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
